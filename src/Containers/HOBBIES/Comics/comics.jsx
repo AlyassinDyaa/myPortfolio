1bf6e51dc39a82d4b203fdec3995e73bf9aa3b20
Dyaa Alyassin <dyaaalyassin@yahoo.com>
2023-03-04 23:34:44 -0500

    added comic card for comics section of hobbies

--- src/Containers/HOBBIES/Comics/comics.jsx
+++ src/Containers/HOBBIES/Comics/comics.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import "./comics.css"
+import orephic from "../../../Assets/infinites/orephic.png"
+import orephicB from "../../../Assets/infinites/o.PNG"
 
 const comics = () => {
   return (
-    <div>
-      
-  </div>
+    <div className='container__comics'>
+          <a href="https://www.mythrillfiction.com/the-dark-rider" alt="Mythrill" target="_blank">
+
+              <div class="cardC">
+                <div class="wrapper">
+                  <img src={orephicB} class="cover-image" />
+                </div>
+                {/* <img src="https://ggayane.github.io/css-experiments/cards/dark_rider-title.png" class="title" /> */}
+                <img src={orephic} class="character" />
+              </div>
+          </a>
+
+    </div>
+
+
+
+
   )
 }
 
